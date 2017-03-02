999b98355b6a3c47e7ac806c33fd8e33da456aad
Erik Liodden <eriklio@stud.ntnu.no>
2017-03-02 21:13:30 +0100

    Parser: Nicer format for long messages
    Long messages will now (hopefully) have a nicer format when displayed to
    user. Previously the string would wrap at the end of the terminal and
    kind of "overlap" with the info column (time and names). It didn't look
    nice, and it felt out of place.
    
    Now if the text is too long, it will wrap down to the next ':' sign,
    below the first, and continue from there. The width is set to ~80
    characters.
    
    Also added some minor cosmetic changes.

--- client/message_parser.py
+++ client/message_parser.py
@@ -2,6 +2,14 @@
 import json
 import datetime
 
+def calculate(string):
+    # split the string anc calculate lenght of the words
+    o = string.split()
+    l = []
+    for word in o:
+        l.append(len(word))
+    return o, l
+
 class MessageParser():
     def __init__(self):
 
@@ -27,21 +35,34 @@ class MessageParser():
 
     def parse_error(self, payload):
         # Parse the error message
-        print("{:>10}: {}".format("error", payload['content']))
+        print(" {:>10}: {}".format("error", payload['content']))
 
     def parse_info(self, payload):
         # Parse the info message
-        print("{:>10}: {}".format("info", payload['content']))
+        print(" {:>10}: {}".format("info", payload['content']))
 
     def parse_message(self, payload):
-        # Parse the message itself
-        print("{:>10}: {}".format(payload['sender'], payload['content']))
+        # Parse the message itself and create a formated string
+        # that is (hopefull) shorter than the terminal default window
+        string, lengths = calculate(payload['content'])
+        counter = 0
+        s = ""
+        for i in range(len(string)):
+            if counter + lengths[i] > 58:
+                s += "\n                   : "
+                counter = 0
+            counter += lengths[i] + 1
+            s += string[i] + ' '
+        print(" {:>10}: {}".format(payload['sender'], s))
 
     def parse_history(self, payload):
         # Parse a history-response
-        print("========== History ==========")
+        print(" : ============================== History " +
+            "==============================")
         for message in payload['content']:
             msg = json.loads(message)
             time = datetime.datetime.fromtimestamp(msg['timestamp'])
             print("{}".format(time.strftime('%H:%M:%S')), end='')
             self.parse_message(msg)
+        print("-------- : ===============================" +
+                "======================================")
