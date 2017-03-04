561817ae28107fead684a108ec0bc6db526cc91b
Erik Liodden <eriklio@stud.ntnu.no>
2017-03-04 17:42:05 +0100

    Server: Remove trailing blanks
    Slighter cleaner code without trailing blanks. No changes in
    functionality, only cosmetic changes in source code.

--- server/server.py
+++ server/server.py
@@ -7,8 +7,10 @@ import re
 Variables and functions that must be used by all the ClientHandler objects
 must be written here (e.g. a dictionary for connected clients)
 """
+
 u = {}
 log = []
+
 class ClientHandler(socketserver.BaseRequestHandler):
     """
     This is the ClientHandler class. Everytime a new client connects to the
@@ -27,8 +29,8 @@ class ClientHandler(socketserver.BaseRequestHandler):
             'msg': self.parse_msg,
             'names': self.parse_names,
             'help': self.parse_help,
-            # More key:values pairs are needed
         }
+
         self.d = {}
         self.ip = self.client_address[0]
         self.port = self.client_address[1]
@@ -42,47 +44,43 @@ class ClientHandler(socketserver.BaseRequestHandler):
             except:
                 self.parse_logout()
                 return 0
+
             self.d = {}
             received['request'] = received['request'].lower()
-            print(received)
             if(received['request'] in self.possible_request):
                 self.possible_request[received['request']](received)
             else:
                 self.response_error("request invalid")
-            
+
             if(received['request'] == 'logout'):
                 return 0
-            
-            # TODO: Add handling of received payload from client
-    
-
 
     def response_error(self, cont):
         self.d['sender'] = 'server'
         self.d['response'] = "error"
         self.d['content'] = cont
         self.send()
-        
+
     def parse_login(self, data):
         if(self.connection in u.keys()):
             return self.response_error("Already logged in")
-            
+
         if(data['content'] in u.values()):
             return self.response_error("Username taken")
-            
-        if(data['content'] == "" or not(re.match('[a-zA-Z0-9]*$',data['content']))):
+
+        if(data['content'] == "" or not(re.match('[a-zA-Z0-9]*$', data['content']))):
             return self.response_error("Username invalid")
+
         u[self.connection] = data['content']
         self.d['sender'] = 'server'
         self.d['response'] = 'history'
         self.d['content'] = log
         self.send()
-            
-    
+
     def parse_logout(self, data = None, user = None):
         if(self.connection not in u.keys()):
             return self.response_error("Not logged in")
-            
+
         self.d['sender'] = 'server'
         self.d['response'] = 'info'
         self.d['content'] = 'logout successful'
@@ -90,43 +88,41 @@ class ClientHandler(socketserver.BaseRequestHandler):
             user = self.connection
         u.pop(user, None)
         user.close()
-        
-        
+
     def parse_msg(self, data):
         if(self.connection not in u.keys()):
             return self.response_error("Not logged in")
-            
+
         self.d['sender'] = u[self.connection]
         self.d['response'] = 'message'
         self.d['content'] = data['content']
         self.d['timestamp'] = time.time()
         log.append(json.dumps(self.d))
         self.send()
-    
+
     def parse_names(self, data):
         if(self.connection not in u.keys()):
             return self.response_error("Not logged in")
         s = ""
         for l in u.values():
             s += (l+" ")
-        print(s)
         self.d['content'] = s
         self.d['sender'] = 'server'
         self.d['response'] = 'info'
         self.send()
-    
+
     def parse_help(self, data):
-        h = ("login<username> - login to server\n"+
+        h = ("\nlogin<username> - login to server\n"+
         "logout - logout of server\n"+
         "msg<message> - Send a message to connected clients\n"+
         "names - list all usernames connected to server\n"+
         "help - show this help text")
-        
+
         self.d['response'] = 'info'
         self.d['content'] = h
         self.d['sender'] = 'server'
         self.send()
-    
+
     def send(self):
         #convert to json and send
         self.d['timestamp'] = time.time()
@@ -142,7 +138,6 @@ class ClientHandler(socketserver.BaseRequestHandler):
                 self.parse_logout('', user)
         else:
             self.connection.send(payload.encode())
-    
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     """
